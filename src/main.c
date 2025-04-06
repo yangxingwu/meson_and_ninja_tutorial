f8564d658a8d2bd01c2efbc0e942fa34e3e65344
yangxingwu <xingwu.yang@gmail.com>
2025-04-06 21:37:42 +0800

    initial commit

---
+++ src/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "max/max.h"
+#include "min/min.h"
+
+int main(int argc, char **argv) {
+  printf("The max of integer 1 and 2 is: %d\n", max(1, 2));
+  printf("The min of integer 1 and 2 is: %d\n", min(1, 2));
+  return 0;
+}
---
+++ subprojects/libmin/min.c
@@ -0,0 +1,5 @@
+#include "min/min.h"
+
+int min(int a, int b) {
+  return a < b ? a : b;
+}
